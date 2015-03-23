043d7043933f51cfecd427736e01b81c2a939290
markootasevic <motasevic994@gmail.com>
2015-03-23 19:34:40 +0100

    implementirane metode iz klase Menjacnica koja implementira interfejs

--- src/menjacnica/Menjacnica.java
+++ src/menjacnica/Menjacnica.java
@@ -1,27 +1,54 @@
 package menjacnica;
 
-import java.util.GregorianCalendar;
+import java.util.GregorianCalendar; 
+import java.util.LinkedList;
 
 import menjacnica.interfejs.MenjacnicaInterfejs;
 
 public class Menjacnica implements MenjacnicaInterfejs {
 
-	@Override
-	public void dodajKurs(double prodajni, double kupovni) {
-		// TODO Auto-generated method stub
+	public LinkedList<Valuta> valuta = new LinkedList<Valuta>();
 
+	@Override
+	public void dodajKurs(double prodajni, double kupovni,
+			GregorianCalendar dan, String naziv, String skraceniNaziv) {
+		Valuta v = new Valuta();
+		v.setDatum(dan);
+		v.setKupovniKurs(kupovni);
+		v.setProdajniKurs(prodajni);
+		v.setNaziv(naziv);
+		v.setSkraceniNaziv(skraceniNaziv);
+		double srednji = (prodajni + kupovni) / 2;
+		v.setSrednjiKurs(srednji);
+		valuta.add(v);
 	}
 
 	@Override
-	public void brisanjeKursa(GregorianCalendar dan) {
-		// TODO Auto-generated method stub
-
+	public void brisanjeKursa(GregorianCalendar dan, String naziv) {
+		Valuta v = new Valuta();
+		v.setDatum(dan);
+		v.setNaziv(naziv);
+		for (int i = 0; i < valuta.size(); i++) {
+			if(valuta.get(i).equals(v))
+				valuta.remove(i);
+		}
+		
 	}
 
 	@Override
-	public void pronadjiKursNaDan(GregorianCalendar dan) {
-		// TODO Auto-generated method stub
-
+	public void pronadjiKursNaDan(GregorianCalendar dan, String naziv) {
+		Valuta v = new Valuta();
+		v.setDatum(dan);
+		v.setNaziv(naziv);
+		for(int i = 0;i < valuta.size(); i++) {
+			if(valuta.get(i).equals(v))
+				System.out.println(valuta.get(i));
+		}
+		
 	}
+
+	
+
+	
 
 }
