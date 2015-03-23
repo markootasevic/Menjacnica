e299b6bab8df60a08c4805eef2f15a8b4b47fd37
markootasevic <motasevic994@gmail.com>
2015-03-23 19:42:44 +0100

    Izmenjen interfejs i dodati neophodni ulazni parametri u metode sto su propusteni prvi put kada su pravljene metode.

--- src/menjacnica/Menjacnica.java
+++ src/menjacnica/Menjacnica.java
@@ -1,27 +1,39 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import menjacnica.interfejs.MenjacnicaInterfejs;
 
 public class Menjacnica implements MenjacnicaInterfejs {
 
+	public LinkedList<Valuta> valuta = new LinkedList<Valuta>();
 	@Override
-	public void dodajKurs(double prodajni, double kupovni) {
-		// TODO Auto-generated method stub
-
+	public void dodajKurs(double prodajni, double kupovni,
+			GregorianCalendar dan, String naziv, String skraceniNaziv) {
+		Valuta v1 = new Valuta();
+		v1.setDatum(dan);
+		v1.setKupovniKurs(kupovni);
+		v1.setNaziv(naziv);
+		v1.setProdajniKurs(prodajni);
+		v1.setSkraceniNaziv(skraceniNaziv);
+		v1.setSrednjiKurs((kupovni + prodajni) / 2);
+		valuta.add(v1);
+		
 	}
 
 	@Override
-	public void brisanjeKursa(GregorianCalendar dan) {
+	public void brisanjeKursa(GregorianCalendar dan, String naziv) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
-	public void pronadjiKursNaDan(GregorianCalendar dan) {
+	public void pronadjiKursNaDan(GregorianCalendar dan, String naziv) {
 		// TODO Auto-generated method stub
-
+		
 	}
+
+	
 
 }
--- src/menjacnica/interfejs/MenjacnicaInterfejs.java
+++ src/menjacnica/interfejs/MenjacnicaInterfejs.java
@@ -4,7 +4,7 @@ import java.util.GregorianCalendar;
 
 public interface MenjacnicaInterfejs {
 	
-	public void dodajKurs(double prodajni,double kupovni);
-	public void brisanjeKursa(GregorianCalendar dan);
-	public void pronadjiKursNaDan(GregorianCalendar dan);
+	public void dodajKurs(double prodajni,double kupovni,GregorianCalendar dan,String naziv,String skraceniNaziv);
+	public void brisanjeKursa(GregorianCalendar dan,String naziv);
+	public void pronadjiKursNaDan(GregorianCalendar dan,String naziv);
 }
